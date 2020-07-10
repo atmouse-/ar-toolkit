9e0ce4820aecbd8402bbbe07eb283fb5bbf3415e
jingjiezhuang <jingjie.zhuang@igg.com>
2020-07-10 10:09:11 +0800

    drop local tempfile

--- daily-bing.py
+++ daily-bing.py
@@ -10,6 +10,7 @@ import json
 import hashlib
 import fnmatch
 
+from io import BytesIO
 from PIL import Image
 from libqtile.sh import QSh
 from libqtile import command
@@ -59,14 +60,14 @@ def limit_color(color):
         return "#C8C8C8"
     return color
 
-def fetch_bing_bg(path='/tmp'):
+def fetch_bing_bg():
     html = urllib.request.urlopen("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US").read().decode('utf-8')
     _j = json.loads(html)
     _url = _j['images'][0]['url']
     url = "http://cn.bing.com" + _url
-    xpath = path + '/bing_bg.jpg'  
-    print(xpath)
-    urllib.request.urlretrieve(url, xpath)  
+    return BytesIO(
+        urllib.request.urlopen(url).read()
+    )
 
 def sha256sum(filepath):
     BUF_SIZE = 65536
@@ -77,6 +78,18 @@ def sha256sum(filepath):
             if not data:
                 break
             sha256.update(data)
+    return sha256.hexdigest()
+
+def sha256sum_io(fd):
+    BUF_SIZE = 65536
+    sha256 = hashlib.sha256()
+    fd.seek(0)
+    while True:
+        data = fd.read(BUF_SIZE)
+        if not data:
+            break
+        sha256.update(data)
+    fd.seek(0)
     return sha256.hexdigest()
 
 def clean_oguri_wallpaper():
@@ -99,12 +112,14 @@ if __name__ == '__main__':
     #home = expanduser("~")
     #if os.path.isfile(home + "/.config/qtile/wall.jpg"):
     #    shutil.copyfile(home + "/.config/qtile/wall.jpg", "/tmp/bing_bg.jpg")
-    fetch_bing_bg()
+    bg_image = fetch_bing_bg()
     end=time.time()  
     print('done %.2f seconds' % (end-start))
     clean_oguri_wallpaper()
-    wallpaper = "/home/atmouse/.config/oguri/wallpaper/{}.jpg".format(sha256sum("/tmp/bing_bg.jpg"))
-    shutil.move("/tmp/bing_bg.jpg", wallpaper)
+    wallpaper = "/home/atmouse/.config/oguri/wallpaper/{}.jpg".format(sha256sum_io(bg_image))
+    with open(wallpaper, "wb") as f:
+        f.write(bg_image.read())
+        f.flush()
     #sway = Sway()
     #sway.sway_set_bg(wallpaper)
     oguri_set_bg(wallpaper)
