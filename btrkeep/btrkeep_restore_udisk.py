6d4ace18f0fe37ead5ca9486e74cca234f6e166e
jingjiezhuang <jingjie.zhuang@igg.com>
2020-10-10 22:29:21 +0800

    btrkeep: add luks crypt support

--- btrkeep/btrkeep_restore_udisk.py
+++ btrkeep/btrkeep_restore_udisk.py
@@ -43,9 +43,27 @@ def get_root_uuid():
     result = result.decode('utf-8').strip()
     return result
 
+def prepare_udisk(uuid):
+    retcode = os.system("mount -o noatime,nodiratime /dev/disk/by-uuid/{} /mnt/udisk".format(uuid))
+    if retcode != 0:
+        print("mount udisk fs error")
+        sys.exit(1)
+
+def prepare_udisk_luks(uuid):
+    cmd = "cryptsetup open /dev/disk/by-uuid/{} root-lexar".format(uuid)
+    retcode = os.system(cmd)
+    if retcode != 0:
+        print("decrypt udisk error")
+        sys.exit(1)
+    retcode = os.system("mount -o subvolid=5 /dev/disk/by-id/dm-name-root-lexar /mnt/udisk")
+    if retcode != 0:
+        print("mount decrypt udisk error")
+        sys.exit(1)
+
 if __name__ == "__main__":
     ROOT_UUID = get_root_uuid()
-    UDISK_UUID = "7A1971D87759BD20"
+    UDISK_UUID = "7342e6e4-39ee-48c2-9012-e5e54bce7c5b"
+    UDISK_TYPE = "crypto_LUKS"
 
     result = subprocess.check_output("blkid", shell=True)
     if result.find(ROOT_UUID.encode('utf-8')) < 0:
@@ -64,23 +82,36 @@ if __name__ == "__main__":
         print("please unmount /mnt/udisk/")
         sys.exit(1)
     os.system("mount -o subvolid=5 /dev/disk/by-uuid/{} /mnt/root".format(ROOT_UUID))
-    os.system("mount -o subvolid=5 /dev/disk/by-uuid/{} /mnt/udisk".format(UDISK_UUID))
-    
+    if UDISK_TYPE == "crypto_LUKS":
+        prepare_udisk_luks(UDISK_UUID)
+    else:
+        prepare_udisk(UDISK_UUID)
+
     root_snapshots = [f for f in listdir("/mnt/root") if f.startswith("rootfs-")]
-    udisk_snapshots = [f for f in listdir("/mnt/udisk/rootfs") if f.startswith("rootfs-")]
+    udisk_snapshots = [f for f in listdir("/mnt/udisk") if f.startswith("rootfs-")]
     _mixmax_snapshot = get_last_snapshot(list(set(root_snapshots).intersection(udisk_snapshots)))
     prepare_snapshots = [f for f in udisk_snapshots if f > _mixmax_snapshot]
     _last_snapshot = get_last_snapshot(udisk_snapshots)
 
     if prepare_snapshots:
-        for _snapshot in prepare_snapshots:
-            cmd = "zcat /mnt/udisk/rootfs/{1}/{0}.gz | btrfs receive /mnt/root/".format(_mixmax_snapshot, _snapshot)
-            print(cmd)
-            print("Press Enter to continue:", end="")
-            input()
-            os.system(cmd)
-            _mixmax_snapshot = _snapshot
-            os.system("sync")
+        if UDISK_TYPE == "crypto_LUKS":
+            for _snapshot in prepare_snapshots:
+                cmd = "btrfs send -p /mnt/udisk/{0} /mnt/udisk/{1} | btrfs receive /mnt/root/".format(_mixmax_snapshot, _snapshot)
+                print(cmd)
+                print("Press Enter to continue:", end="")
+                input()
+                os.system(cmd)
+                _mixmax_snapshot = _snapshot
+                os.system("sync")
+        else:
+            for _snapshot in prepare_snapshots:
+                cmd = "zcat /mnt/udisk/{1}/{0}.gz | btrfs receive /mnt/root/".format(_mixmax_snapshot, _snapshot)
+                print(cmd)
+                print("Press Enter to continue:", end="")
+                input()
+                os.system(cmd)
+                _mixmax_snapshot = _snapshot
+                os.system("sync")
         # 
         print("please:")
         print("create clone of {} to a new writeable snapshot as activefs-2019100811111-rw".format(_last_snapshot))
@@ -92,3 +123,6 @@ if __name__ == "__main__":
 
     os.system("umount /mnt/root")
     os.system("umount /mnt/udisk")
+
+    if UDISK_TYPE == "crypto_LUKS":
+        os.system("cryptsetup close root-lexar")
--- btrkeep/btrkeep_save_udisk.py
+++ btrkeep/btrkeep_save_udisk.py
@@ -43,9 +43,27 @@ def get_root_uuid():
     result = result.decode('utf-8').strip()
     return result
 
+def prepare_udisk(uuid):
+    retcode = os.system("mount -o noatime,nodiratime /dev/disk/by-uuid/{} /mnt/udisk".format(uuid))
+    if retcode != 0:
+        print("mount udisk fs error")
+        sys.exit(1)
+
+def prepare_udisk_luks(uuid):
+    cmd = "cryptsetup open /dev/disk/by-uuid/{} root-lexar".format(uuid)
+    retcode = os.system(cmd)
+    if retcode != 0:
+        print("decrypt udisk error")
+        sys.exit(1)
+    retcode = os.system("mount -o subvolid=5 /dev/disk/by-id/dm-name-root-lexar /mnt/udisk")
+    if retcode != 0:
+        print("mount decrypt udisk error")
+        sys.exit(1)
+
 if __name__ == "__main__":
     ROOT_UUID = get_root_uuid()
-    UDISK_UUID = "7A1971D87759BD20"
+    UDISK_UUID = "7342e6e4-39ee-48c2-9012-e5e54bce7c5b"
+    UDISK_TYPE = "crypto_LUKS"
 
     result = subprocess.check_output("blkid", shell=True)
     if result.find(ROOT_UUID.encode('utf-8')) < 0:
@@ -64,22 +82,34 @@ if __name__ == "__main__":
         print("please unmount /mnt/udisk/")
         sys.exit(1)
     os.system("mount -o subvolid=5 /dev/disk/by-uuid/{} /mnt/root".format(ROOT_UUID))
-    os.system("mount -o noatime,nodiratime /dev/disk/by-uuid/{} /mnt/udisk".format(UDISK_UUID))
+    if UDISK_TYPE == "crypto_LUKS":
+        prepare_udisk_luks(UDISK_UUID)
+    else:
+        prepare_udisk(UDISK_UUID)
     
     root_snapshot = [f for f in listdir("/mnt/root") if f.startswith("rootfs-")]
-    udisk_snapshot = [f for f in listdir("/mnt/udisk/rootfs") if f.startswith("rootfs-")]
+    udisk_snapshot = [f for f in listdir("/mnt/udisk") if f.startswith("rootfs-")]
     _mixmax_snapshot = get_last_snapshot(root_snapshot and udisk_snapshot)
     _last_snapshot = get_last_snapshot(root_snapshot)
 
     if _last_snapshot > _mixmax_snapshot:
-        os.mkdir("/mnt/udisk/rootfs/{}".format(_last_snapshot))
-        cmd = "btrfs send -p /mnt/root/{0} /mnt/root/{1} | gzip > /mnt/udisk/rootfs/{1}/{0}.gz".format(_mixmax_snapshot, _last_snapshot)
-        print(cmd)
-        os.system(cmd)
-        os.system("sync")
-        os.system("gzip -v -t /mnt/udisk/rootfs/{1}/{0}.gz".format(_mixmax_snapshot, _last_snapshot))
+        if UDISK_TYPE == "crypto_LUKS":
+            cmd = "btrfs send -p /mnt/root/{0} /mnt/root/{1} | btrfs receive /mnt/udisk/".format(_mixmax_snapshot, _last_snapshot)
+            print(cmd)
+            os.system(cmd)
+            os.system("sync")
+        else:
+            os.mkdir("/mnt/udisk/{}".format(_last_snapshot))
+            cmd = "btrfs send -p /mnt/root/{0} /mnt/root/{1} | gzip > /mnt/udisk/{1}/{0}.gz".format(_mixmax_snapshot, _last_snapshot)
+            print(cmd)
+            os.system(cmd)
+            os.system("sync")
+            os.system("gzip -v -t /mnt/udisk/{1}/{0}.gz".format(_mixmax_snapshot, _last_snapshot))
     else:
         print("nothing to do")
 
     os.system("umount /mnt/root")
     os.system("umount /mnt/udisk")
+
+    if UDISK_TYPE == "crypto_LUKS":
+        os.system("cryptsetup close root-lexar")
